89b03c6fcd9c45828bbd63c3968e72307a2b542b
King Cobra <kcobra557@gmail.com>
2020-03-16 02:42:48 +0000

    Added more user related profile object

--- js/main.js
+++ js/main.js
@@ -14,9 +14,21 @@ $(document).ready(function () {
 				<div class="card-body">
 					<div class="row">
 						<div class="col-md-3">
-							<img class="thumbnail avatar" src="${user.avatar_url}">
+							<img class="thumbnail avatar" src="${user.avatar_url}"><br><br>
+							<a target="_blank" class="btn btn-primary btn-block" href="${user.html_url}">View Profile</a>
 						</div>
 						<div class="col-md-9">
+							<span class="badge badge-pill badge-secondary">Public Repos: ${user.public_repos}</span>
+							<span class="badge badge-pill badge-success">Public Gists: ${user.public_gists}</span>
+							<span class="badge badge-pill badge-danger">Followers: ${user.followers}</span>
+							<span class="badge badge-pill badge-warning">Following: ${user.following}</span>
+							<br><br>
+							<ul class="list-group list-group-flush">
+								<li class="list-group-item">Company: ${user.company}</li>
+								<li class="list-group-item">Website/Blog: ${user.blog}</li>
+								<li class="list-group-item">Location: ${user.location}</li>
+								<li class="list-group-item">Member Since: ${user.created_at}</li>
+							</ul>
 						</div>
 					</div>
 				</div>
