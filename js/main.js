01b2143450409211f9fb408a7c052c4cb8722987
King Cobra <kcobra557@gmail.com>
2020-03-16 02:13:47 +0000

    Added bootstrap card to call api elements in place

--- js/main.js
+++ js/main.js
@@ -1,13 +1,26 @@
-$(document).ready(function(){
-	$('#searchUser').on('keyup', function(e){
+$(document).ready(function () {
+	$('#searchUser').on('keyup', function (e) {
 		let username = e.target.value;
-		
+
 		// Make request to GitHub
 		$.ajax({
-			url:'https://api.github.com/users/'+username
-		}).done(function(user){
+			url: 'https://api.github.com/users/' + username
+		}).done(function (user) {
 			$('#profile').html(`
-				${user.name}
+			<div class="card">
+				<div class="card-header">
+					${user.name}
+				</div>
+				<div class="card-body">
+					<div class="row">
+						<div class="col-md-3">
+							<img class="thumbnail avatar" src="${user.avatar_url}">
+						</div>
+						<div class="col-md-9">
+						</div>
+					</div>
+				</div>
+			</div>
 			`);
 		});
 	});
